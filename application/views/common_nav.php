c55d37976b79cfd2fd44ec184e2695c60086d2b8
jiangbianwanghai <jiangbianwanghai@163.com>
2013-05-02 20:47:37 +0800

    删除无用的文件

--- application/views/common_nav.php
+++
@@ -1,5 +0,0 @@
-<ul class="breadcrumb">
-  <li><a href="#">首页</a> <span class="divider">/</span></li>
-  <li><a href="#">手机号管理</a> <span class="divider">/</span></li>
-  <li class="active">发布手机号</li>
-</ul>
